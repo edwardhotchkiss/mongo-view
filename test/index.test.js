e5c16f180b8fb3cd25df18c8efa985f8a3cca6a7
Edward Hotchkiss <edward@edwardhotchkiss.com>
2012-01-16 20:14:16 -0500

    [tests] check http server with request

--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,11 @@
   Core Modules
  */
 
-var vows = require('vows'),
-    assert = require('assert'),
-    mongodb_viewer = require('../lib/mongodb-viewer');
+var vows = require('vows')
+    , assert = require('assert')
+    , request = require('request')
+    , mongodb_viewer = require('../lib/mongodb-viewer')
+    , httpServer = mongodb_viewer.httpServer;
 
 /*!
   vows / `npm test`
@@ -19,6 +21,19 @@ vows.describe('general module tests').addBatch({
     'mongodb_viewer should be an object':function(topic) {
       assert.isObject(topic);
     },
+  },
+  'when starting then requesting the application':{
+    topic:function(){
+      var self = this;
+      mongodb_viewer.run();
+      request('http://localhost:8000/', self.callback);
+    },
+    'request should return no errors, a 200 response code, and html within the body':function(error, response, body){
+      httpServer.close();
+      assert.isNull(error);
+      assert.equal(response.statusCode, 200);
+      assert.equal(/html/m.test(body), true);
+    }
   }
 }).export(module);
 
