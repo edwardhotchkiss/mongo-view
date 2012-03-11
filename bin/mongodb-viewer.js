50f809001678814fd2a1db51b866b2f44350850c
Edward Hotchkiss <edward@edwardhotchkiss.com>
2012-03-11 09:52:22 -0400

    [bin] commander fix!

--- bin/mongodb-viewer.js
+++ bin/mongodb-viewer.js
@@ -1,38 +1,15 @@
 #!/usr/bin/env node
 
-/*!
-
-http://github/edwardhotchkiss/mongodb-viewer/
-Node.JS MongoDB web-based admin/viewer tool
-
-*/
-
-/*!
-  Module Dependencies
+/**
+ * Module Dependencies
  */
 
 require('../app/helpers/colors');
 
-var program = require('commander')
-  , mongodb_viewer = require('../app/lib/mongodb-viewer');
-
-/*!
-  Setup CLI w/ Commander.js
- */
-
-program
-  .version('0.1.2')
-
-program
-  .command('run [port]')
-  .description('run mongodb-viewer on <port>')
-  .option("-p, --port [port]", 'Which port to run mongodb-viewer on')
-  .action(function(port){
-    port = port || 8000;
-    console.log(['\n','> running mongodb-viewer on port ',port,'\n'].join('').cyan);
-    mongodb_viewer.app.listen(port);
-  });
-
-program.parse(process.argv);
+var mongodb_viewer = require('../app/lib/mongodb-viewer')
+  , port = port || 8000;
+    
+console.log(['\n','> running mongodb-viewer on port ',port,'\n'].join('').cyan);
+mongodb_viewer.app.listen(port);
 
 /* EOF */
