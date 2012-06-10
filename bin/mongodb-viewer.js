6b45157549275f99752e56d3c9ab64103fa81730
Edward Hotchkiss <edwardhotchkiss@me.com>
2012-06-10 14:15:00 -0400

    [main] removed

--- bin/mongodb-viewer.js
+++ bin/mongodb-viewer.js
@@ -1,15 +1,28 @@
 #!/usr/bin/env node
 
 /**
- * Module Dependencies
- */
+ * @list dependencies
+ **/
 
 require('../app/helpers/colors');
 
 var mongodb_viewer = require('../app/lib/mongodb-viewer')
+  , args = process.argv
   , port = port || 8000;
-    
-console.log(['\n','> running mongodb-viewer on port ',port,'\n'].join('').cyan);
-mongodb_viewer.app.listen(port);
+
+console.log(args);
+
+switch(args[2]) {
+  case 'run':
+  case '-r':
+  case '--run':
+    console.log(['\n','> running mongodb-viewer on port ',port,'\n'].join('').cyan);
+    mongodb_viewer.app.listen(port);
+    break;
+  default:
+    logger('No command specified!'.yellow);
+    process.exit(0);
+    break;
+};
 
 /* EOF */
