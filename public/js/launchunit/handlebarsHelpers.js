f4a44c9b4335142eba238f460c97ea7d74d1c2fa
Edward Hotchkiss <edwardhotchkiss@me.com>
2012-06-24 18:12:12 -0400

    [helpers] added pushIdToTop (of obj) method

--- public/js/launchunit/handlebarsHelpers.js
+++ public/js/launchunit/handlebarsHelpers.js
@@ -6,6 +6,19 @@
 
 function registerHandlebarsHelpers(self) {
 
+  /**
+   * @method pushIdToTop
+   **/
+
+  function pushIdToTop(obj) {
+    var ordered = {};
+    ordered['_id'] = obj['_id'];
+    for (key in obj) {
+      ordered[key] = obj[key];
+    };
+    return ordered;
+  };
+
   // get database name
   Handlebars.registerHelper('getDB', function() {
     return self.db;
@@ -19,27 +32,21 @@ function registerHandlebarsHelpers(self) {
   // prettify a mongodb item
   Handlebars.registerHelper('generateRow', function(item) {
     var HTML = '';
-    var props = item[0];
-    var orderedProps = {};
-    // push _id to the top
-    orderedProps['_id'] = props['_id'];
-    for (key in props) {
-      orderedProps[key] = props[key];
-    };
+    var orderedItem = pushIdToTop(item[0]);
     // sort through and classify
-    for (key in orderedProps) {
+    for (key in orderedItem) {
       HTML += '<div class="holder">';
       HTML += '<div class="key left"><p>';
       HTML += key;
       HTML += '</p></div>'
       HTML += '<div class="prop left"><p>';
-      if (typeof(props[key]) === 'number') {
-        HTML += '<span class="identifier-number">' + props[key] + '</span>';
-      } else if (typeof(props[key]) === 'string') {
+      if (typeof(orderedItem[key]) === 'number') {
+        HTML += '<span class="identifier-number">' + orderedItem[key] + '</span>';
+      } else if (typeof(orderedItem[key]) === 'string') {
         if (key === '_id') {
-          HTML += '<span class="identifier-objectid">$' + props[key] + '</span>';
+          HTML += '<span class="identifier-objectid">$' + orderedItem[key] + '</span>';
         } else {
-          HTML += '<span class="identifier-string">"' + props[key] + '"</span>';
+          HTML += '<span class="identifier-string">"' + orderedItem[key] + '"</span>';
         };
       };
       HTML += '</p></div>';
@@ -47,5 +54,5 @@ function registerHandlebarsHelpers(self) {
     };
     return new Handlebars.SafeString(HTML);
   });
-  
+
 };
